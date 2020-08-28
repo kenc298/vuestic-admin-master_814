432605afbc9d1c81ce733aa1a9f21db0165ac059
cyiu-yoyo <70261592+cyiu-yoyo@users.noreply.github.com>
2020-08-28 16:47:50 +0800

    Update example.spec.js

--- unit/example.spec.js
+++ unit/example.spec.js
@@ -2,6 +2,6 @@ describe('App', () => {
   // Mock test is required so that jest does not complain we have no tests on job.
   it('runs a mock test', () => {
     expect(true).toBe(true)
-    console.log('app')
+    //console.log('app')
   })
 })
